546babe2698ae75dc040e18b294540e2f42e9077
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-29 11:51:06 +0900

    cart_productsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129024144) do
+ActiveRecord::Schema.define(version: 20171129024850) do
+
+  create_table "cart_products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "product_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
