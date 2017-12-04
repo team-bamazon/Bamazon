1736fed2b2ed58ee3fade92410184bfbd586f3f0
drumnaoto <drumnaoto@gmail.com>
2017-12-04 11:06:57 +0900

    edit-index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,7 @@
-$(function() {
+$(document).on('turbolinks:load', function () {
   $('.a-carousel').slick({
     accessibility: true,
     autoplay: 3000,
     arrows: true,
-    // appendArrows: $("#arrows")
   });
 });
