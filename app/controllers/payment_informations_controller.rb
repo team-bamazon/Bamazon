9ef309a9340fc9f802f08d48036d0e54d2f14915
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-12-08 12:17:19 +0900

    payment_infomationのビュー追加

--- app/controllers/payment_informations_controller.rb
+++ app/controllers/payment_informations_controller.rb
@@ -3,4 +3,12 @@ class PaymentInformationsController < ApplicationController
   def index
   end
 
+  def create
+  end
+
+  private
+  def payment_information_params
+    params.require(:payment_information).permit(:card_name, :card_number, :month, :year, :full_name, :postal_code_one, :postal_code_two, :region, )
+  end
+
 end
