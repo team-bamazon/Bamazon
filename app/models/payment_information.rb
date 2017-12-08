9d6caa1cfcb8f9cc93f6aab0d2d154523f635642
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-12-08 21:07:11 +0900

    ビューの修正とクレジットの表示ナンバー制限

--- app/models/payment_information.rb
+++ app/models/payment_information.rb
@@ -1,3 +1,19 @@
 class PaymentInformation < ApplicationRecord
   belongs_to :user
+  validates :card_name, presence: true
+  validates :card_number, presence: true
+  validates :month, presence: true
+  validates :year, presence: true
+  validates :postal_code_one, presence: true
+  validates :postal_code_two, presence: true
+  validates :region, presence: true
+  validates :street_address_one, presence: true
+  validates :building_name, presence: true
+  validates :phone_number, presence: true
+  validates :full_name, presence: true
+
+  def number_method
+    number = card_number.split("")
+    return  number[12] + number[13] + number[14] + number[15]
+  end
 end
