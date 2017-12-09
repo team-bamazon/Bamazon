65101d91fc3f0b4290d67c167a954a314e07b86a
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-12-09 17:13:08 +0900

    編集機能の追加

--- app/controllers/payment_informations_controller.rb
+++ app/controllers/payment_informations_controller.rb
@@ -6,7 +6,7 @@ class PaymentInformationsController < ApplicationController
   end
 
   def create
-    @card = PaymentInformation.new(payment_information_params)
+    @card = PaymentInformation.new(payment_information_params.merge(user_id: params[:id]))
     if @card.save
       redirect_to user_payment_informations_path(current_user)
     else
@@ -18,12 +18,23 @@ class PaymentInformationsController < ApplicationController
   def destroy
     payment_information = PaymentInformation.find(params[:id])
     payment_information.destroy if payment_information.user_id == current_user.id
-    redirect_to user_payment_informations_path
+    redirect_to user_payment_informations_path(current_user)
+  end
+
+  def edit
+    @card = PaymentInformation.find(params[:id])
+  end
+
+  def update
+    @card = PaymentInformation.find(params[:id])
+    @card.update(payment_information_params)
+    redirect_to user_payment_informations_path(current_user)
   end
 
   private
     def payment_information_params
-      params.require(:payment_information).permit(:card_name, :card_number, :month, :year, :full_name, :postal_code_one, :postal_code_two, :region, :street_address_one, :street_address_two, :building_name, :phone_number).merge(user_id: params[:id])
+      params.require(:payment_information).permit(:card_name, :card_number, :month, :year, :full_name, :postal_code_one, :postal_code_two, :region, :street_address_one, :street_address_two, :building_name, :phone_number)
+      # params.require(:payment_information).permit(:card_name, :card_number, :month, :year, :full_name, :postal_code_one, :postal_code_two, :region, :street_address_one, :street_address_two, :building_name, :phone_number).merge(user_id: params[:id])
     end
 
 end
