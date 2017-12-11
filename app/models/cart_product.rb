2331a5c53ac8d4ee603177cf9b119a0191e61836
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-12-11 17:29:11 +0900

    Create carts_controller

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,24 @@
 class CartProduct < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  def product_price_sum(cart_product)
+    cart_products = CartProduct.where(cart_id: cart_product.cart_id)
+    sum = 0
+    cart_products.each do |cart_item|
+      price_sum =  cart_item.product.price * cart_item.product_count
+      sum += price_sum
+    end
+    return sum.floor.to_s(:delimited)
+  end
+
+  def product_count_sum(cart_product)
+    cart_products = CartProduct.where(cart_id: cart_product.cart_id)
+    sum = 0
+    cart_products.each do |cart_item|
+      sum += cart_item.product_count.to_i
+    end
+    return sum.floor.to_s(:delimited)
+  end
+
 end
