9b9a4e232f558dfbae7daa143c8f0df1eaaede01
teslur <teslur@gmail.com>
2017-12-12 19:24:48 +0900

    ログインがこける問題を解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_cart, if: :user_signed_in?
-
-  protect_from_forgery with: :exception
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :furigana])
