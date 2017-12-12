270409afcb0561b7eb3e3a8379e4b2cc87b9937d
teslur <teslur@gmail.com>
2017-12-12 14:42:06 +0900

    商品の評価値を取得するメソッドを作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,24 @@ class Product < ApplicationRecord
   belongs_to :product_category
 
   accepts_nested_attributes_for :product_images
+
+  def get_star_value
+    # productインスタンスに紐づくreviewについたrateの平均値を返すメソッド
+    # reviewがない場合はゼロを返す
+    # 返す値は0-10の範囲の整数値
+    if self.reviews
+      # countをfloat型で扱わせるために1.0をかけている
+      count = self.reviews.count * 1. 0
+      star_value_sum = 0;
+      self.reviews.each do |r|
+        star_value_sum += r.rate;
+      end
+      # 戻り値は平均値の切り上げ
+      star_value = (star_value_sum / count).ceil
+    else
+      star_value = 0
+    end
+
+    return star_value
+  end
 end
