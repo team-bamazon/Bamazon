2f2bfd27534aebc61ff13fcc74613eb42a9e7f1e
teslur <teslur@gmail.com>
2017-12-12 19:43:49 +0900

    turbolinks対応

--- app/assets/javascripts/suggest.js
+++ app/assets/javascripts/suggest.js
@@ -1,29 +1,30 @@
-$(function() {
-    var suggest_field = $("#suggestions-template");
-    var prev_input    = "";
-    var input_width = 0;
-    var suggest_flg = false;
+$(document).on('turbolinks:load', function() {
+    $(function () {
+        var suggest_field = $("#suggestions-template");
+        var prev_input = "";
+        var input_width = 0;
+        var suggest_flg = false;
 
-    function appendSuggest(suggest) {
-        var html =`<div class="s-suggestion"><span>${ suggest.name }</span></div>`;
-        return html;
-    }
+        function appendSuggest(suggest) {
+            var html = `<div class="s-suggestion"><span>${ suggest.name }</span></div>`;
+            return html;
+        }
 
-    $("#twotabsearchtextbox").on("keyup", function() {
-        var input = $("#twotabsearchtextbox").val().trim();
+        $("#twotabsearchtextbox").on("keyup", function () {
+            var input = $("#twotabsearchtextbox").val().trim();
 
-        // 直前の入力と現在の入力が異なる場合再検索（行頭・末尾のスペース入力時対策）
-        if(prev_input != input) {
-            prev_input = input;
+            // 直前の入力と現在の入力が異なる場合再検索（行頭・末尾のスペース入力時対策）
+            if (prev_input != input) {
+                prev_input = input;
 
-            // 検索文字列が存在するときのみ検索
-            if(input != "") {
-                $.ajax({
-                    type: 'GET',
-                    url: '/products/suggest',
-                    data: {keyword: input},
-                    dataType: 'json'
-                })
+                // 検索文字列が存在するときのみ検索
+                if (input != "") {
+                    $.ajax({
+                        type: 'GET',
+                        url: '/products/suggest',
+                        data: {keyword: input},
+                        dataType: 'json'
+                    })
 
                     .done(function (suggests) {
                         if (suggests.length != 0) {
@@ -48,30 +49,31 @@ $(function() {
                     .fail(function () {
                         alert('サジェスト検索に失敗しました');
                     })
+                }
+                // 検索文字列がない場合、前回の検索結果の削除のみ実施
+                else {
+                    suggest_field.empty();
+                    $("#nav-flyout-searchAjax").attr("style", "display: none;");
+                    suggest_flg = false;
+                }
             }
-            // 検索文字列がない場合、前回の検索結果の削除のみ実施
-            else {
-                suggest_field.empty();
-                $("#nav-flyout-searchAjax").attr("style", "display: none;");
-                suggest_flg = false;
-            }
-        }
-    });
+        });
 
-    $(document).on("click", ".s-suggestion", function() {
-        var keyword = $(this).text();
-        console.log(keyword);
-        $("#twotabsearchtextbox").val(keyword);
-        $('#search_btn').trigger("click");
-    });
+        $(document).on("click", ".s-suggestion", function () {
+            var keyword = $(this).text();
+            console.log(keyword);
+            $("#twotabsearchtextbox").val(keyword);
+            $('#search_btn').trigger("click");
+        });
 
-    $(window).resize(function() {
-        if(suggest_flg){
-            if(input_width != $("#twotabsearchtextbox").innerWidth()){
-                suggest_field.empty();
-                $("#nav-flyout-searchAjax").attr("style", "display: none;");
-                suggest_flg = false;
+        $(window).resize(function () {
+            if (suggest_flg) {
+                if (input_width != $("#twotabsearchtextbox").innerWidth()) {
+                    suggest_field.empty();
+                    $("#nav-flyout-searchAjax").attr("style", "display: none;");
+                    suggest_flg = false;
+                }
             }
-        }
+        });
     });
 });
