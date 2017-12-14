a02ddd9ba846c80d31c5c2ca9d062e418d3daac3
teslur <teslur@gmail.com>
2017-12-14 11:24:06 +0900

    最終更新日付を画面に表示

--- app/controllers/wanteds_controller.rb
+++ app/controllers/wanteds_controller.rb
@@ -3,8 +3,10 @@ class WantedsController < ApplicationController
     @wanteds = User.find(params[:user_id]).wanteds
     @selected_wanted = Wanted.find(params[:id])
 
-    @wanted_products = @selected_wanted.products
+    @selected_wanted_products = @selected_wanted.products
     @selected_id = @selected_wanted.id
     @user_id = params[:user_id]
+
+    @update_datetimes = Hash[@selected_wanted.wanted_products.map{|wp| [wp.product_id, wp.updated_at.strftime("%m月 %d %Y")]}]
   end
 end
