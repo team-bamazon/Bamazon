b39c5910c328a2cd6539bd09db655c9a8faa5fcc
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-12-15 18:37:49 +0900

    不要な記述の削除

--- app/models/wanted.rb
+++ app/models/wanted.rb
@@ -2,24 +2,4 @@ class Wanted < ApplicationRecord
   belongs_to :user
   has_many   :wanted_products
   has_many   :products, through: :wanted_products
-# カートと同じように処理を定義
-  # def wanted_informations(wanted)
-  #   wanted.wanted_products
-  # end
-
-  # def wanted_price_sum(wanted)
-  #   sum = 0
-  #   wanted_informations(wanted).each do |wanted_info|
-  #     sum  += wanted_info.product.price * wanted_info.product_count
-  #   end
-  #   return sum.floor.to_s(:delimited)
-  # end
-
-  # def wanted_count_sum(cart)
-  #   sum = 0
-  #   wanted_informations(wanted).each do |wanted_count|
-  #     sum  += wanted_count.product_count
-  #   end
-  #   return sum.floor.to_s(:delimited)
-  # end
 end
--- app/models/wanted_product.rb
+++ app/models/wanted_product.rb
@@ -1,33 +1,4 @@
 class WantedProduct < ApplicationRecord
   belongs_to :product
   belongs_to :wanted
-# cart_productsと同じような処理を記述
-#   def product_price_sum(wanted_product)
-#     cart_products = WantedProduct.where(wanted_id: wanted_product.wanted_id)
-#     sum = 0
-#     wanted_products.each do |wanted_item|
-#       price_sum =  wanted_item.product.price * wanted_item.product_count
-#       sum += price_sum
-#     end
-#     return sum.floor.to_s(:delimited)
-#   end
-
-#   def product_count_sum(wanted_product)
-#     wanted_products = WantedProduct.where(wanted_id: wanted_product.wanted_id)
-#     sum = 0
-#     wanted_products.each do |wanted_item|
-#       sum += wanted_item.product_count.to_i
-#     end
-#     return sum.floor.to_s(:delimited)
-#   end
-
-#   def bamazon_point(wanted_product)
-#     wanted_products = WantedProduct.where(wanted_id: wanted_product.wanted_id)
-#     sum = 0
-#     wanted_products.each do |wanted_item|
-#       price_sum =  wanted_item.product.price * wanted_item.product_count
-#       sum += price_sum
-#     end
-#     return sum.floor
-#   end
 end
