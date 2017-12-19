6a2d40a90bb935a791bdbc7efafb01bdd802644a
drumnaoto <drumnaoto@gmail.com>
2017-12-19 16:52:52 +0900

    improve-cart_product_controller
    非ログイン時のための修正。

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -9,7 +9,6 @@ class CartProductsController < ApplicationController
       @cart_product = CartProduct.find_by(product_id: cart_product_params[:product_id], cart_id: cart_product_params[:cart_id])
       sum = @cart_product[:product_count] + cart_product_params[:product_count].to_i
       @cart_product.update(product_count: sum)
-
     else
       @cart_product = CartProduct.new(cart_product_params)
       @cart_product.save
@@ -30,7 +29,11 @@ class CartProductsController < ApplicationController
   private
 
   def cart_product_params
-    params.require(:cart_product).permit(:product_count).merge(cart_id: @cart.id, product_id: params[:product_id] )
+    if user_signed_in?
+      params.require(:cart_product).permit(:product_count).merge(cart_id: current_user.cart.id, product_id: params[:product_id])
+    else
+      params.require(:cart_product).permit(:product_count).merge(cart_id: get_current_cart.id, product_id: params[:product_id])
+    end
   end
 
 end
