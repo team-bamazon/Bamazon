7f0f55877d80fc951cbb1ebf425894aff17e8450
teslur <teslur@gmail.com>
2017-12-19 17:49:35 +0900

    コメント追加＆不要なbinding.pry削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,23 +11,27 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :furigana])
   end
 
+  # @cartにuserに紐づいたcartをセットする処理。ログイン済状態でのみ呼ばれる関数。
   def set_cart
+    # cartが作成済の場合（過去にログインしたことがあるユーザーの場合）の処理
     if current_user.cart.present?
+      # 作成済みcartをuser_cartに設定
       @cart = current_user.cart
       user_cart = @cart
-      binding.pry
+
+      # current_cart(未ログイン時に利用するカート)の存在有無で処理分岐
+      # 存在する場合、中身を作成済cartに移す。
       current_cart = self.get_current_cart
-      # binding.pry
       if current_cart.present?
-        # binding.pry
+        # current_cart(未ログイン時に利用するカート)の中身あったらcartに移す、空だったら何もしない
         if current_cart.cart_products.present?
-          # binding.pry
           current_cart.cart_products.each do |product|
+            # current_cart(未ログイン時に利用するカート)の中身が作成済cartにもある場合、作成済cartの中の個数を増やす
             if user_cart.cart_products.find_by(product_id: product.product_id).present?
-              # binding.pry
               user_cart_product =  user_cart.cart_products.find_by(product_id: product.product_id)
               sum = user_cart_product.product_count + product.product_count
               user_cart_product.update(product_count: sum)
+            # current_cart(未ログイン時に利用するカート)の中身が作成済cartにない場合、作成済cartにproductを入れる
             else
               @cart_product = CartProduct.new(
                 product_id: product.product_id,
@@ -38,16 +42,21 @@ class ApplicationController < ActionController::Base
             end
           end
         end
+
+        # cartに中身を移したら、current_cart(未ログイン時に利用するカート)はもう不要なので削除。
         session[:cart_id] = nil
-        binding.pry
         current_cart.destroy
       end
+    # cartが未作成の場合（今回が初回ログインのユーザーの場合）の処理
     else
-      binding.pry
+      # 未ログイン時に作成されたcurrent_cartが存在する場合の処理
       if get_current_cart
+        # current_cartにuser_idを設定し、永続的なcartにして@cartに設定
         @cart = get_current_cart
         @cart.update(user_id: current_user.id)
+      # 未ログイン時に作成されたcurrent_cartが存在しない場合の処理（通常状態ではありえないはずの処理）
       else
+        # userに紐づくcartを新規作成し、@cartに設定
         @cart = Cart.create(user_id: current_user.id)
       end
     end
@@ -61,11 +70,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # 未ログイン時に利用するcartを@cartに設定する処理。ログイン時も未ログイン時も呼ばれうるが、未ログイン時のみ動く。ログイン時は何もしない。
   def set_current_cart
+    # 未ログイン時のみ稼働
     unless user_signed_in?
-      # binding.pry
+      # このセッション中で既に作成したcartがある場合（session[:cart_id]に値が入っている場合）、それを@cartに設定する
       if session[:cart_id]
         @cart = Cart.find(session[:cart_id])
+      # このセッション中で作成したcartが無い場合（このセッションで初めてこの処理を呼ぶ場合）、cartを作成し@cartとsession[:cart_id]にcart_idを設定する。
       else
         @cart = Cart.create
         session[:cart_id] = @cart.id
@@ -73,8 +85,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # session[:cart_id]に値がある場合（＝未ログイン時）に呼ばれた場合、そのcart_idでcartを探して返す。
+  # ログイン時はnilを返すので、ログイン後に呼ぶ可能性がある箇所では呼び出し後にnil判定必須！！！
   def get_current_cart
-    # binding.pry
     session[:cart_id].present? ? Cart.find(session[:cart_id]) : nil
   end
 
