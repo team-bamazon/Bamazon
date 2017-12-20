6f83793f7224e5d4086b5fb9d1df3a633d0372e3
teslur <teslur@gmail.com>
2017-12-20 14:37:32 +0900

    ☆の割合を出す処理、描画する処理を簡略化

--- app/models/product.rb
+++ app/models/product.rb
@@ -36,64 +36,19 @@ class Product < ApplicationRecord
 
     return {star_value: star_value.round(1), star_value_css: star_value_css}
   end
-# 星が5の時の全体の割合を返す処理
-  def rate_average_five
-    if self.reviews.present?
-      count = self.reviews.count
-      # rateが10のものがいくつあるか探す処理
-      star_value_sum = self.reviews.where(rate: 10).count
-      # rate10のレビューの%をだす処理
-      star_value = (star_value_sum * 100) / count
-    else
-      star_value = 0
-    end
-  end
-# 星が4の時の全体の割合を返す処理
-  def rate_average_four
-    if self.reviews.present?
-      count = self.reviews.count
-      # rateが8のものがいくつあるか探す処理
-      star_value_sum = self.reviews.where(rate: 8).count
-      # rate8のレビューの%をだす処理
-      star_value = (star_value_sum * 100) / count
-    else
-      star_value = 0
-    end
-  end
-# 星が3の時の全体の割合を返す処理
-  def rate_average_three
-    if self.reviews.present?
-      count = self.reviews.count
-      # rateが6のものがいくつあるか探す処理
-      star_value_sum = self.reviews.where(rate: 6).count
-      # rate6のレビューの%をだす処理
-      star_value = (star_value_sum * 100) / count
-    else
-      star_value = 0
-    end
-  end
-# 星が2の時の全体の割合を返す処理
-  def rate_average_two
-    if self.reviews.present?
-      count = self.reviews.count
-      # rateが4のものがいくつあるか探す処理
-      star_value_sum = self.reviews.where(rate: 4).count
-      # rate4のレビューの%をだす処理
-      star_value = (star_value_sum * 100) / count
-    else
-      star_value = 0
-    end
-  end
-# 星が1の時の全体の割合を返す処理
-  def rate_average_one
+
+  # 星がrateの時の全体の割合を返す処理
+  def rate_average(rate)
     if self.reviews.present?
       count = self.reviews.count
       # rateが2のものがいくつあるか探す処理
-      star_value_sum = self.reviews.where(rate: 2).count
+      star_value_sum = self.reviews.where(rate: rate).count
       # rate2のレビューの%をだす処理
       star_value = (star_value_sum * 100) / count
     else
       star_value = 0
     end
+
+    return star_value
   end
 end
