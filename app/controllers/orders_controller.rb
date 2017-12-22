6b4afce028d56c7fbabe2dab9008f74697f6e6e1
drumnaoto <drumnaoto@gmail.com>
2017-12-22 17:10:24 +0900

    edit-orders_controller
    showアクションをeditに変更。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,28 @@
 class OrdersController < ApplicationController
 
-  def create
-    # @order = Order.find(params[:id])
-    Address.create(full_name:          @order.full_name,
-                   postal_code_one:    @order.postal_code_one,
-                   postal_code_two:    @order.postal_code_two,
-                   region:             @order.region,
-                   street_address_one: @order.street_address_one,
-                   building_name:      @order.building_name,
-                   phone_number:       @order.phone_number)
-    binding.pry
-    redirect_to order_path
-  end
+  # def create
+  #   # @order = Order.find(params[:id])
+  #   Address.create(full_name:          @order.full_name,
+  #                  postal_code_one:    @order.postal_code_one,
+  #                  postal_code_two:    @order.postal_code_two,
+  #                  region:             @order.region,
+  #                  street_address_one: @order.street_address_one,
+  #                  building_name:      @order.building_name,
+  #                  phone_number:       @order.phone_number)
+  #   binding.pry
+  #   redirect_to order_path
+  # end
 
-  def show
+  def edit
     @order = Order.find(params[:id])
-    @new_order = Order.new
+    @address = Address.new(user_id: current_user.id)
+    # @new_order = Order.new
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    redirect_to order_path
+    # redirect_to order_path
   end
 
   private
