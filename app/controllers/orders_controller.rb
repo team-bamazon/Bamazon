7942ef350bb39463368a11f2cad70b8fb47a8f7a
drumnaoto <drumnaoto@gmail.com>
2017-12-23 14:18:02 +0900

    modify-orders_controller
    ordersコントローラーを、
    updateアクションが機能するように修正。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,21 +6,29 @@ class OrdersController < ApplicationController
   end
 
   def edit
+    binding.pry
     @order = Order.find(params[:id])
     @address = Address.new
     @card = PaymentInformation.new
-
-    # @new_order = Order.new
   end
 
   def update
     @order = Order.find(params[:id])
-    # @order.update(order_params)
-    # redirect_to order_path
+    @order.update(status: 1,
+                  country: current_user.addresses.last.country,
+                  full_name: current_user.addresses.last.full_name,
+                  postal_code_one: current_user.addresses.last.postal_code_one,
+                  postal_code_two: current_user.addresses.last.postal_code_two,
+                  region: current_user.addresses.last.region,
+                  street_address_one: current_user.addresses.last.street_address_one,
+                  street_address_two: current_user.addresses.last.street_address_two,
+                  building_name: current_user.addresses.last.building_name,
+                  phone_number: current_user.addresses.last.phone_number,
+                  card_name: current_user.payment_informations.last.card_name,
+                  card_number: current_user.payment_informations.last.card_number,
+                  month: current_user.payment_informations.last.month,
+                  year: current_user.payment_informations.last.year)
+    @cart.destroy
   end
 
-  private
-  def order_params
-    params.require(:order).permit(:full_name, :postal_code_one, :postal_code_two, :region, :street_address_one, :building_name, :phone_number).merge(user_id: current_user.id)
-  end
 end
