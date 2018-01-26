b8c39478a60cc26f24a8ba1461fd60fcb28f29b1
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2018-01-26 17:47:44 +0900

    Modify address_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,22 @@
 class AddressesController < ApplicationController
   def new
-    path = Rails.application.routes.recognize_path(request.referer)
-    if path[:controller] == "addresses"
-      @address = Address.new
-    else
-      respond_to do |format|
-        format.html
-        format.json
-      end
-    end
+    @address = Address.new
+    # 下記のはいらないと思い削除
+    # path = Rails.application.routes.recognize_path(request.referer)
+    # if path[:controller] == "addresses"
+    #   @address = Address.new
+    # else
+    #   respond_to do |format|
+    #     format.html
+    #     format.json
+    #   end
+    # end
   end
 
   def create
-    @address = Address.create(address_params)
+    @change_address = Address.find_by(status: 1, user_id: current_user.id)
+    @change_address.update(status: 0) if @change_address.present?
+     @address = Address.create(address_params)
     path = Rails.application.routes.recognize_path(request.referer)
     if path[:controller] == "addresses"
       redirect_to user_edit_address_path
@@ -42,11 +46,14 @@ class AddressesController < ApplicationController
 
   def update_status
     @address = Address.find(params[:id])
-    @change_address = Address.find_by(status: 1)
+    # user_idもないと他の人のアドレスが取られる可能性があるので一応
+    @change_address = Address.find_by(status: 1, user_id: current_user.id)
     @address.update(status: 1)
-    @change_address.update(status: 0)
+    # @change_addressが空のことも考えられるのでif分岐追加
+    @change_address.update(status: 0) if @change_address.present?
     path = Rails.application.routes.recognize_path(request.referer)
-    if path[:controller] == "addresses"
+    # addressではなくusersだったので変更
+    if path[:controller] == "users"
       redirect_to user_edit_address_path
     else
       respond_to do |format|
@@ -59,6 +66,7 @@ class AddressesController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:country, :full_name, :postal_code_one, :postal_code_two, :region,:street_address_one, :street_address_two, :building_name, :phone_number).merge(user_id: params[:user_id])
+    params.require(:address).permit(:country, :full_name, :postal_code_one, :postal_code_two, :region,:street_address_one, :street_address_two, :building_name, :phone_number, :status).merge(user_id: params[:user_id])
+    # アドレスが作られる時にstatusも保存しておいた方がいいので追加
   end
 end
